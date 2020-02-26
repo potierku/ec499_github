e5b33f152f7d53fa34ef4dad82fb44a8150826d4
potierku <you@example.com>
2020-02-26 00:34:49 -0500

    .

--- data_analysis.R
+++ data_analysis.R
@@ -18,7 +18,7 @@ yrng <- range(df_melted$value)
 plot<-ggplot(data=df_melted,aes(x=year,y=value,shape=variable))+
   geom_line()+
   geom_point()+
-  labs(shape="Countries",y=y,title=title)+
+  labs(shape="Countries",y=y,title=title,x="Year")+
   theme(plot.margin = margin(0.5,0.5,0.5,0.5,"cm"),plot.background = element_rect(colour="black"),
         axis.text.x = element_text(angle =45),plot.title = element_text(hjust=0.5))+
   scale_x_continuous(breaks=unique(df_melted$year))
@@ -56,7 +56,7 @@ grapher(title,df,y)
 #######GDP Growth###############
 df <- data.frame(armenia$year,czech_republic$gdp_growth,
                  ukraine$gdp_growth,armenia$gdp_growth)
-title <- "GDP Growth by Year"
+title <- "Figure 1: GDP Growth by Year"
 y <- "GDP growth %"
 grapher(title,df,y)
 
@@ -70,35 +70,49 @@ grapher(title,df,y)
 #######inflation############
 df <- data.frame(armenia$year,czech_republic$inflation_cpi,
                  ukraine$inflation_cpi,armenia$inflation_cpi)
-title <- "INflation by Year"
+title <- "Figure 2: Inflation by Year"
+y<- "Inflation %"
+grapher(title,df,y)
+
+#######inflation_early############
+df <- data.frame(armenia$year[1:10],czech_republic$inflation_cpi[1:10],
+                 ukraine$inflation_cpi[1:10],armenia$inflation_cpi[1:10])
+title <- "Figure 3: Inflation by Year (1992-1996)"
+y<- "Inflation %"
+grapher(title,df,y)
+
+#######inflation_late############
+df <- data.frame(armenia$year[10:40],czech_republic$inflation_cpi[10:40],
+                 ukraine$inflation_cpi[10:40],armenia$inflation_cpi[10:40])
+title <- "Figure 4: Inflation by Year (1996-2018)"
 y<- "Inflation %"
 grapher(title,df,y)
 
 ###########unemployment###########
 df <- data.frame(armenia$year,czech_republic$unemployment,
                  ukraine$unemployment,armenia$unemployment)
-title <- "Unemployment by Year"
+title <- "Figure 5: Unemployment by Year"
 y<- "Unemployment %"
 grapher(title,df,y)
 
 ##########government debt as a percentage of GDP########
 df <- data.frame(armenia$year,czech_republic$debt_gdp,
                  ukraine$debt_gdp,armenia$debt_gdp)
-title <- "Government Debt as a Percentage of GDP"
+title <- "Figure 6: Government Debt as a Percentage of GDP"
 y<- "Government Debt %"
 grapher(title,df,y)
 
 ############FDI as a percentage of GDP############
 df <- data.frame(armenia$year,czech_republic$fdi_gdp,
                  ukraine$fdi_gdp,armenia$fdi_gdp)
-title <- "FDI as a Percentage of GDP"
+title <- "Figure 8: FDI as a Percentage of GDP"
 y<- "FDI Debt %"
 grapher(title,df,y)
 
 ###########Gross Capital formation as a percentage of GDP########
 df <- data.frame(armenia$year,czech_republic$capital_gdp,
                  ukraine$capital_gdp,armenia$capital_gdp)
-title <- "Gross Capital Formation as a Percentage of GDP"
+title <- "Figure 7: Gross Capital Formation as a Percentage of GDP"
 y<- "Gross Capital Formation %"
 grapher(title,df,y)
 
